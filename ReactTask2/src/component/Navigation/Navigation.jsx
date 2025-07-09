75b1fa9f12aaf6009a676efc5a296ec5c81bc55c
RiteshPatil7097 <157485127+RiteshPatil7097@users.noreply.github.com>
2025-07-09 18:46:45 +0530

    Create Navigation.jsx

---
+++ ReactTask2/src/component/Navigation/Navigation.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from './Navigation.module.css'
+
+const Navigation = () => {
+  console.log(styles);
+
+  return (
+    <nav className={`${styles.Navigation} container`}>
+      <div className='logo'>
+           <img src="/images/logo.png" alt="this is a image"></img>
+      </div>
+      
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+
+    </nav>
+  )
+}
+
+export default Navigation;
